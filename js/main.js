eb60d863552a391d671ffdb212beff252f34c506
insssomniac <42058438+insssomniac@users.noreply.github.com>
2020-09-14 15:29:25 +0300

    refactor menu and added toggle menu function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 const menuButton = document.querySelector("#hamburger");
 const fullscreenMenu = document.querySelector("#fullscreen-menu");
 const closeFsMenu = document.querySelector("#close-fs-menu");
+const menuLinks = fullscreenMenu.querySelectorAll(".menu__link");
 
-menuButton.addEventListener('click', e => {
-  e.preventDefault();
-  fullscreenMenu.style.display = 'block';
-  menuButton.style.display = 'none';
-});
+function toggleMenu() {
+  menuButton.classList.toggle('hamburger--active');
+  fullscreenMenu.classList.toggle('fullscreen-menu--active')
+}
 
-closeFsMenu.addEventListener('click', e => {
-  e.preventDefault();
-  fullscreenMenu.style.display = 'none';
-  menuButton.style.display = '';
+menuButton.addEventListener('click', toggleMenu);
+closeFsMenu.addEventListener('click', toggleMenu);
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', toggleMenu);
 });
