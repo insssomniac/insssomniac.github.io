3d9523431dd4cc0bed21cf25c495614c371814c2
insssomniac <42058438+insssomniac@users.noreply.github.com>
2020-09-28 12:20:38 +0300

    map fixed

--- js/map.js
+++ js/map.js
@@ -1,10 +1,20 @@
 let map;
 
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 59.939095, lng: 30.315868 },
+        zoom: 12,
+        disableDefaultUI: true,
+    });
+
+    setMarkers(map);
+}
+
 const coords = [
-    [59.94554327989287, 30.38935262114668],
-    [59.91142323563909, 30.50024587065841],
-    [59.88693161784606, 30.319658102103713],
-    [59.97033574821672, 30.315194906302924],
+    {lat: 59.94554327989287, lng: 30.38935262114668},
+    {lat: 59.91142323563909, lng: 30.50024587065841},
+    {lat: 59.88693161784606, lng: 30.319658102103713},
+    {lat: 59.97033574821672, lng: 30.315194906302924}
 ];
 
 function setMarkers(map) {
@@ -15,25 +25,17 @@ function setMarkers(map) {
         // The origin for this image is (0, 0).
         origin: new google.maps.Point(0, 0),
         // The anchor for this image is the base of the flagpole at (0, 32).
-        anchor: new google.maps.Point(0, 32)
+        anchor: new google.maps.Point(23, 57),
     };
 
     for (let i = 0; i < coords.length; i++) {
         let point = coords[i];
         let marker = new google.maps.Marker({
-            position: {lat: parseFloat(point[1]), lng: parseFloat(point[2])},
+            position: point,
             map: map,
             icon: markerIcon,
         });
     }
+
 }
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 59.939095, lng: 30.315868 },
-        zoom: 12,
-        disableDefaultUI: true,
-    });
-
-    setMarkers(map);
-}
